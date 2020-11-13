c00bf63a36d0c95bf4ccfae39ab5ce320636e374
Edi Selimi <selimi.edi.1991@gmail.com>
2020-11-13 13:25:01 +0100

    style changes

--- js/layout.js
+++ js/layout.js
@@ -1,49 +1,52 @@
 var Layout = function () {
     'use strict';
-    
+
+    //  Cookies Popup
+   
+
     // handle on page scroll
-    var handleHeaderOnScroll = function() {
+    var handleHeaderOnScroll = function () {
         if ($(window).scrollTop() > 60) {
             $('body').addClass('page-on-scroll');
         } else {
             $('body').removeClass('page-on-scroll');
         }
-    }   
+    }
 
     // handle carousel
-    var handleCarousel = function() {
-        var $item = $('.carousel .item'); 
+    var handleCarousel = function () {
+        var $item = $('.carousel .item');
         var $wHeight = $(window).height();
         $item.eq(0).addClass('active');
-        $item.height($wHeight); 
+        $item.height($wHeight);
         $item.addClass('full-screen');
 
-        $('.carousel img').each(function() {
+        $('.carousel img').each(function () {
             var $src = $(this).attr('src');
             var $color = $(this).attr('data-color');
             $(this).parent().css({
-                'background-image' : 'url(' + $src + ')',
-                'background-color' : $color
+                'background-image': 'url(' + $src + ')',
+                'background-color': $color
             });
             $(this).remove();
         });
 
-        $(window).on('resize', function (){
+        $(window).on('resize', function () {
             $wHeight = $(window).height();
             $item.height($wHeight);
         });
     }
 
     // handle group element heights
-    var handleHeight = function() {
-       $('[data-auto-height]').each(function() {
+    var handleHeight = function () {
+        $('[data-auto-height]').each(function () {
             var parent = $(this);
             var items = $('[data-height]', parent);
             var height = 0;
             var mode = parent.attr('data-mode');
             var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
 
-            items.each(function() {
+            items.each(function () {
                 if ($(this).attr('data-height') == "height") {
                     $(this).css('height', '');
                 } else {
@@ -58,7 +61,7 @@ var Layout = function () {
 
             height = height + offset;
 
-            items.each(function() {
+            items.each(function () {
                 if ($(this).attr('data-height') == "height") {
                     $(this).css('height', height);
                 } else {
@@ -66,10 +69,10 @@ var Layout = function () {
                 }
             });
 
-            if(parent.attr('data-related')) {
+            if (parent.attr('data-related')) {
                 $(parent.attr('data-related')).css('height', parent.height());
             }
-       });
+        });
     }
 
     return {
@@ -79,7 +82,7 @@ var Layout = function () {
             handleHeight(); // initial setup for group element height
 
             // handle minimized header on page scroll
-            $(window).scroll(function() {
+            $(window).scroll(function () {
                 handleHeaderOnScroll();
             });
         }
@@ -88,91 +91,91 @@ var Layout = function () {
 
 // Gradient Generator
 var colors = new Array(
-    [0,0,0],
-    [8,8,8],
-    [16,16,16],
-    [24,24,24],
-    [32,32,32],
-    [40,40,40],
-    [48,48,48],
-    [56,56,56],
-    [64,64,64],
-    [72,72,72],
-    [80,80,80],
-    [88,88,88],
-    [96,96,96],
-    [112,112,112],
-    [120,120,120],
-    [128,128,128],
-    [136,136,136],
-    [144,144,144],
-    [152,152,152],
-    [160,160,160],
-    [168,168,168],
-    [176,176,176],
-    [184, 184,184],
-    [190,190,190],
-    [198,198,198],
-    [208,208,208],
-    [216,216,216],
-    [224,224,224],
-    [232,232,323],
-    [240,240,240],
-    [245,245,245],
-    [248,248,248],
-    [255,255,255],
-    );
-  
-  var step = 0;
-  //color table indices for: 
-  // current color left
-  // next color left
-  // current color right
-  // next color right
-  var colorIndices = [0,1,2,3];
-  
-  //transition speed
-  var gradientSpeed = 0.06;
-  
-  function updateGradient()
-  {
-  var c0_0 = colors[colorIndices[0]];
-  var c0_1 = colors[colorIndices[1]];
-  var c1_0 = colors[colorIndices[2]];
-  var c1_1 = colors[colorIndices[3]];
-  
-  var istep = 1 - step;
-  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  var color1 = "#"+((r1 << 16) | (g1 << 8) | b1).toString(16);
-  
-  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
-  
-   $('#description_div').css({
-     background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-      background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-    
-    step += gradientSpeed;
-    if ( step >= 1 )
-    {
-      step %= 1;
-      colorIndices[0] = colorIndices[1];
-      colorIndices[2] = colorIndices[3];
-      
-      //pick two new target color indices
-      //do not pick the same as the current one
-      colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-      colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-      
-    }
-  }
-  
-  setInterval(updateGradient,80);
+    [0, 0, 0],
+    [8, 8, 8],
+    [16, 16, 16],
+    [24, 24, 24],
+    [32, 32, 32],
+    [40, 40, 40],
+    [48, 48, 48],
+    [56, 56, 56],
+    [64, 64, 64],
+    [72, 72, 72],
+    [80, 80, 80],
+    [88, 88, 88],
+    [96, 96, 96],
+    [112, 112, 112],
+    [120, 120, 120],
+    [128, 128, 128],
+    [136, 136, 136],
+    [144, 144, 144],
+    [152, 152, 152],
+    [160, 160, 160],
+    [168, 168, 168],
+    [176, 176, 176],
+    [184, 184, 184],
+    [190, 190, 190],
+    [198, 198, 198],
+    [208, 208, 208],
+    [216, 216, 216],
+    [224, 224, 224],
+    [232, 232, 323],
+    [240, 240, 240],
+    [245, 245, 245],
+    [248, 248, 248],
+    [255, 255, 255],
+);
 
-$(document).ready(function() {
+var step = 0;
+//color table indices for: 
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0, 1, 2, 3];
+
+//transition speed
+var gradientSpeed = 0.06;
+
+function updateGradient() {
+    var c0_0 = colors[colorIndices[0]];
+    var c0_1 = colors[colorIndices[1]];
+    var c1_0 = colors[colorIndices[2]];
+    var c1_1 = colors[colorIndices[3]];
+
+    var istep = 1 - step;
+    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+    var color1 = "#" + ((r1 << 16) | (g1 << 8) | b1).toString(16);
+
+    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+    var color2 = "#" + ((r2 << 16) | (g2 << 8) | b2).toString(16);
+
+    $('#description_div').css({
+        background: "-webkit-gradient(linear, left top, right top, from(" + color1 + "), to(" + color2 + "))"
+    }).css({
+        background: "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
+    });
+
+    step += gradientSpeed;
+    if (step >= 1) {
+        step %= 1;
+        colorIndices[0] = colorIndices[1];
+        colorIndices[2] = colorIndices[3];
+
+        //pick two new target color indices
+        //do not pick the same as the current one
+        colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+        colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+
+    }
+}
+
+setInterval(updateGradient, 80);
+
+$(document).ready(function () {
     Layout.init();
 });
